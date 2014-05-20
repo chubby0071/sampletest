66782eb72b0fec7ba41f694a77b6361dd1b940bd
sridhar <sridharp26@gmail.com>
2014-05-20 22:46:42 +0530

    commited
    thanks

--- src/test/java/com/nexia/test.java
+++ src/test/java/com/nexia/test.java
@@ -2,18 +2,21 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
-public class browsertest {
+public class test {
 
+	
+	@Test
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public void verify() {
 		// TODO Auto-generated method stub
  
 		WebDriver driver=new FirefoxDriver();
 		driver.get("https://www.google.com");
-
+		System.out.println("opened");
 	}
 
 }
